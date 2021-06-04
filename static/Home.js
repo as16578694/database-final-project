d425b81885cee8803a729bdb45f0c3f7292ae8ee
annie520143 <annie520143@gmail.com>
2021-06-05 01:14:45 +0800

    js and py api done (need to debug session prob)

--- static/Home.js
+++ static/Home.js
@@ -13,12 +13,33 @@ function createButton(content, height = '60px', width = '140px', setValue = fals
     document.getElementsByClassName("question area")[0].appendChild(btn);
 }
 
+function createDiv(content, height = '60px', width = '140px'){
+    var btn = document.createElement('button');
+    btn.textContent = value;
+    btn.style.height = height
+    btn.style.width = width
+    //btn.style.margin = '15px'
+    btn.style.fontSize = '20px'
+    btn.classList.add("schedule-item")
+    //btn.value = value;
+
+    document.getElementsByClassName("question area")[1].appendChild(btn);
+}
+
 function getPlaceValue(){
     place = this.value;
 }
 
 function getTypeValue(){
     type = this.value;
+}
+
+function getValue(){
+    value = this.value
+}
+
+function getZoneValue(){
+    zone = this.value
 }
 
 var state = 0;
@@ -28,12 +49,26 @@ var state = 0;
 //3 -> 種類
 //4 -> 若是餐廳，則多問時段，其他則3 -> 5
 //5 -> 顯示搜尋結果
-//6 -> 是否完成 No:回到2 Yes:回到0
+//6 -> 繼續選擇 No:回到0 Yes:回到2
 var place = -1;
 //0 -> spot
 //1 -> restaurant
 //2 -> hotel
-var type = -1;
+var type = "";
+var cont = false;
+var value = "";
+var zone = "";
+
+function changeStateNew(){
+    alert("新增成功")
+    var buttonList = document.getElementsByClassName("schedule-item")
+    var n = buttonList.length
+    for(var i=0;i<n;i++)
+        buttonList[0].remove();
+    initial()
+    state = 0
+}
+
 
 function changeState(){
     state++;
@@ -45,18 +80,20 @@ function changeState(){
     switch(state){
         case 1:
             question.innerHTML = "<h3 class='question'>想去新北市的哪個區域?</h3>";
-            createButton("東區 - 瑞芳、平溪", '60px', '200px');
-            createButton("西區 - 新北市區", '60px', '200px');
-            createButton("南區 - 三峽、烏來", '60px', '200px');
-            createButton("北區 - 淡水、北海岸", '60px', '200px');
+            createButton("東區 - 瑞芳、平溪", '60px', '200px', true, 'E');
+            createButton("西區 - 新北市區", '60px', '200px', true, 'W');
+            createButton("南區 - 三峽、烏來", '60px', '200px', true, 'S');
+            createButton("北區 - 淡水、北海岸", '60px', '200px', true, 'N');
+            for(var i = 0; i<buttonList.length;i++)
+                buttonList[i].addEventListener("click", getZoneValue, false)
             break;
 
         case 2 :
             question.innerHTML = "<h3 class='question'>現在想安排景點、用餐、還是住宿?</h3>";
 
-            createButton("景點",height='60px', width = '140px', setValue=true, value=0)
-            createButton("用餐",height='60px', width = '140px', setValue=true, value=1)
-            createButton("住宿",height='60px', width = '140px', setValue=true, value=2)
+            createButton("景點",height='60px', width = '140px', setValue=true, value='S')
+            createButton("用餐",height='60px', width = '140px', setValue=true, value='R')
+            createButton("住宿",height='60px', width = '140px', setValue=true, value='H')
             
             buttonList = document.getElementsByClassName("choice")
             for(var i = 0; i<buttonList.length;i++)
@@ -66,52 +103,71 @@ function changeState(){
         case 3 :
             question.innerHTML = "<h3 class='question'>請選擇想要的類型</h3>";
             //spot
-            if(place == 0){
-                createButton("文化場館",height='60px', width = '140px', setValue=true, value=0)
-                createButton("自然景觀",height='60px', width = '140px', setValue=true, value=1)
-                createButton("宮廟",height='60px', width = '140px', setValue=true, value=2)
-                createButton("老街、夜市",height='60px', width = '140px', setValue=true, value=3)
+            if(place == 'S'){
+                createButton("文化場館",height='60px', width = '140px', setValue=true, value='M')
+                createButton("自然景觀",height='60px', width = '140px', setValue=true, value='N')
+                createButton("宮廟",height='60px', width = '140px', setValue=true, value='T')
+                createButton("老街、夜市",height='60px', width = '140px', setValue=true, value='X')
                 state++;
             }
             //restaurant
-            else if(place == 1){
-                createButton("小點",height='60px', width = '140px', setValue=true, value=0)
-                createButton("中式",height='60px', width = '140px', setValue=true, value=1)
-                createButton("日式",height='60px', width = '140px', setValue=true, value=2)
-                createButton("西式",height='60px', width = '140px', setValue=true, value=3)
-                createButton("泰式",height='60px', width = '140px', setValue=true, value=4)
-                createButton("港式",height='60px', width = '140px', setValue=true, value=5)
-                createButton("韓式",height='60px', width = '140px', setValue=true, value=6)
+            else if(place == 'R'){
+                createButton("小點",height='60px', width = '140px', setValue=true, value='D')
+                createButton("中式",height='60px', width = '140px', setValue=true, value='C')
+                createButton("日式",height='60px', width = '140px', setValue=true, value='J')
+                createButton("西式",height='60px', width = '140px', setValue=true, value='W')
+                createButton("泰式",height='60px', width = '140px', setValue=true, value='T')
+                createButton("港式",height='60px', width = '140px', setValue=true, value='H')
+                createButton("韓式",height='60px', width = '140px', setValue=true, value='K')
                 
             }
             //hotel
-            else if(place == 2){
-                createButton("民宿",height='60px', width = '140px', setValue=true, value=0)
-                createButton("汽車旅館",height='60px', width = '140px', setValue=true, value=1)
-                createButton("旅社",height='60px', width = '140px', setValue=true, value=2)
-                createButton("背包客棧",height='60px', width = '140px', setValue=true, value=3)
-                createButton("飯店/旅店",height='60px', width = '140px', setValue=true, value=4)
+            else if(place == 'H'){
+                createButton("民宿",height='60px', width = '140px', setValue=true, value='A')
+                createButton("汽車旅館",height='60px', width = '140px', setValue=true, value='B')
+                createButton("旅社",height='60px', width = '140px', setValue=true, value='C')
+                createButton("背包客棧",height='60px', width = '140px', setValue=true, value='D')
+                createButton("飯店/旅店",height='60px', width = '140px', setValue=true, value='E')
                 state++;
                 
             }
             buttonList = document.getElementsByClassName("choice")
-            for(var i = 0; i<buttonList.length;i++)
+            for(var i = 0; i<buttonList.length;i++){
                 buttonList[i].addEventListener("click", getTypeValue, false)
+                //若是吃的還有問題要問，跳過
+                if(place != 1){
+                    buttonList[i].addEventListener("click", nametest, false)
+                }
+            }
+            
+            
             break;
 
         case 4:
             question.innerHTML = "<h3 class='question'>請選擇用餐時段</h3>";
-            createButton("早餐",height='60px', width = '140px', setValue=true, value=0)
-            createButton("午餐",height='60px', width = '140px', setValue=true, value=1)
-            createButton("晚餐",height='60px', width = '140px', setValue=true, value=2)
-            createButton("宵夜",height='60px', width = '140px', setValue=true, value=3)
+            createButton("早餐",height='60px', width = '140px', setValue=true, value='0')
+            createButton("午餐",height='60px', width = '140px', setValue=true, value='1')
+            createButton("晚餐",height='60px', width = '140px', setValue=true, value='2')
+            createButton("宵夜",height='60px', width = '140px', setValue=true, value='3')
+
+            buttonList = document.getElementsByClassName("choice")
+            for(var i = 0; i<buttonList.length;i++)
+                buttonList[i].addEventListener("click", nametest, false)
             break;
 
         case 5:
             question.innerHTML = "<h3 class='question'>為您推薦的行程</h3>";
             console.log("顯示搜尋結果");
-            state = 0
             break;
+
+        case 6:
+            question.innerHTML = "<h3 class='question'>您要繼續選擇行程嗎?</h3>";
+            createButton("是",height='60px', width = '140px', setValue=true, value='0');
+            createButton("否",height='60px', width = '140px', setValue=true, value='1');
+            buttonList = document.getElementsByClassName("choice")
+            buttonList[0].addEventListener('click', changeState, false)
+            buttonList[1].addEventListener('click', changeStateNew, false)
+            state = 1;
 
     }
 
@@ -122,9 +178,54 @@ function changeState(){
     
 }
 
-createButton("大眾運輸")
-createButton("自行駕駛")
+function addScheduleItem(){
+    createDiv()
 
-var buttonList = document.getElementsByClassName("choice")
-for(var i = 0; i<buttonList.length;i++)
-    buttonList[i].addEventListener("click", changeState, false)
+}
+
+function nametest() {   
+    
+    //send post to db
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", '/', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        place : place+type,
+        zone : zone
+    }));
+    
+    
+    //response value
+    fetch('http://127.0.0.1:5000/name', {
+      method: 'POST', // or 'PUT'
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(function(response){
+
+        console.log('Success:', response.name)
+        for (var i=0;i<response.name.length;i++){
+            createButton(response.name[i], height='60px', width = '140px', setValue=true, value=response.name[i])
+        }
+        buttonList = document.getElementsByClassName("choice")
+        for(var i = 0; i<buttonList.length;i++){
+            buttonList[i].addEventListener("click", changeState, false)
+            buttonList[i].addEventListener("click", getValue, false)
+            buttonList[i].addEventListener("click", addScheduleItem, false)
+            }
+    });
+}
+
+
+function initial(){
+    createButton("大眾運輸")
+    createButton("自行駕駛")
+
+    var buttonList = document.getElementsByClassName("choice")
+    for(var i = 0; i<buttonList.length;i++)
+        buttonList[i].addEventListener("click", changeState, false)
+}
+
+initial()
--- traveling.py
+++ traveling.py
@@ -1,11 +1,100 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request ,Response,jsonify, session
+from flask_sqlalchemy import SQLAlchemy
+import json
+""" Spot: 文化場館M 自然景觀N 宮廟T 老街、夜市X
+    Restaurant: 小點D 中式C 日式J 西式W 泰式T 港式H 韓式K
+    Hotel:民宿A 汽車旅館B 旅社C 背包客棧D 飯店/旅店E"""
 
 app = Flask(__name__)
+db = SQLAlchemy()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@127.0.0.1:3306/traveling"
+# Setup the secret key and the environment 
+app.config.update(SECRET_KEY='st',
+                  ENV='development')
 
-@app.route("/")
+db.init_app(app)
+
+
+@app.route("/", methods=['GET', 'POST'])
 def test():
+    
+    if(request.method == 'POST'):
+        session.clear()
+        search_request = request.get_json()
+        zone = search_request['zone']
+        place = search_request['place']
+
+        print(zone, place)
+
+        #deal with SQL query
+        select="select Name "
+        frm="from"
+        where="where"
+        query=" "
+        if zone == 'N' : where = where + " "+"zone='N'" + " and "
+        elif zone == 'S' : where = where + " " + "zone='S'" + " and "
+        elif zone == 'W' : where = where + " " + "zone='W'" + " and "
+        elif zone == 'E' : where = where + " " + "zone='E'" + " and "
+           
+        if place[0] == "S" : 
+            frm = frm + " " + "spot "
+            if place[1] == "M" : 
+                where = where + " "+"type='文化場館'" +" and "
+            elif place[1] == "N" : 
+                where = where + " "+"type='自然景觀'" +" and "
+            elif place[1] == "T" : 
+                where = where + " "+"type='宮廟'" +" and "
+            elif place[1] == "X" : 
+                where = where + " "+"type='老街、夜市'" +" and "
+        elif place[0] == "R" : 
+            frm = frm + " "+"restaurant "
+            if place[1] == "D" :  
+                where = where + " "+"type='小點'" +" and "
+            elif place[1] == "C" : 
+                where = where + " "+"type='中式'" +" and "
+            elif place[1] == "J" : 
+                where = where + " "+"type='日式'" +" and "
+            elif place[1] == "W" : 
+                where = where + " "+"type='西式'" +" and "
+            elif place[1] == "T" : 
+                where = where + " "+"type='泰式'" +" and "
+            elif place[1] == "H" : 
+                where = where + " "+"type='港式'" +" and "
+            elif place[1] == "K" : 
+                where = where + " "+"type='韓式'" +" and "
+        else : 
+            frm = frm + " "+"hotel "
+            if place[1] == "A" : 
+                where = where + " "+"type='民宿'" +" and "
+            elif place[1] == "B" : 
+                where = where + " "+"type='汽車旅館'" +" and "
+            elif place[1] == "C" : 
+                where = where + " "+"type='旅社'" +" and "
+            elif place[1] == "D" : 
+                where = where + " "+"type='背包客棧'" +" and "
+            elif place[1] == "E" : 
+                where = where + " "+"type='飯店/旅店'" +" and "
+        query = select + frm + where
+        query=query[:-5]
+        sql_cmd = query
+        print(sql_cmd)
+        query_data = db.engine.execute(sql_cmd+';')
+        myList = []
+        for i,ele in enumerate(query_data.fetchall()):
+            if i>50 : break
+            myList.append(ele[0])
+
+        jsonResult = {}
+        jsonResult['name'] = myList
+        print(myList)
+        session['jsonResult'] = jsonResult
+
     return render_template('Home.html')
-	
+
+@app.route("/name", methods=['POST'])
+def index_name():
+    return Response(json.dumps(session['jsonResult']),mimetype='application/json')
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0",port=5000,debug=True)
